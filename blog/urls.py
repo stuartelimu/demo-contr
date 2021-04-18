4b1abb98c9f9459f4b9db697815ba69b4f6c31ab
stuartelimu <stuartelimu@gmail.com>
2021-04-18 19:47:46 +0300

    create get endpoints with pure django

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import post_list, post_detail
 
 urlpatterns = [
-    
+    path('posts/', post_list, name='post_list'),
+    path('posts/<int:pk>/', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()[:20]
+    data = {'results': list(posts.values('title', 'author__username', 'created_at'))}
+    return JsonResponse(data)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    data = {'results': {
+        'title': post.title,
+        'author': post.author.username,
+        'created_at': post.created_at
+    }}
+    return JsonResponse(data)
